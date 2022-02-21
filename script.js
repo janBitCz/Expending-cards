14d291996e598eefa97106b3998eaec5b3cc8513
janBitCz <bitterlichjan@gmail.com>
2022-02-21 22:21:32 +0100

    add comments

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const panels = document.querySelectorAll('.panel');
 
+//clickable marking function
 panels.forEach( panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
         panel.classList.add('active');  
     })
 })
-
+//function for deleting markings
 const removeActiveClasses = () => {
     panels.forEach( panel => {
         panel.classList.remove('active');
